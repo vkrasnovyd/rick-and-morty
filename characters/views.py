a052cacea0937d767402bd43daebeb431518ffe4
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-02-01 21:14:18 +0100

    Add character-random endpoint

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from random import choice
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from characters.models import Character
+from characters.serializers import CharacterSerializer
+
+
+@api_view(["GET"])
+def get_random_character_view(request) -> Response:
+    pks = Character.objects.values_list("pk", flat=True)
+    random_pk = choice(pks)
+    random_character = Character.objects.get(pk=random_pk)
+    serializer = CharacterSerializer(random_character)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
